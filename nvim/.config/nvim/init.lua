1fe6f56ef02f240746c5ac8cf928b7499395110c
Youngjae Lim <limyoungjae@gmail.com>
2021-10-25 20:21:05 -0400

    Neovim Lua Setup #1 - Syntax Highlighting with Treesitter

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,3 @@
+require('plugins')
+require('treesitter-config')
+vim.cmd('colorscheme nord')
---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,7 @@
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'shaunsingh/nord.nvim'
+  use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"}
+end)
+
