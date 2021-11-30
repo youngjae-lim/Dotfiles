ddd9ea3bb9b6721105dd785adfec4fe10969776f
Youngjae Lim <limyoungjae@gmail.com>
2021-11-30 14:11:45 -0500

    Fixed the issue where ls_emmet is not loaded as a language server after the recent update

--- nvim/.config/nvim/lua/lsp/language_servers.lua
+++ nvim/.config/nvim/lua/lsp/language_servers.lua
@@ -1,19 +1,24 @@
--- Setup lspconfig.
+-- Setup lspconfigo.
+local configs = require 'lspconfig.configs'
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-require'lspconfig/configs'.ls_emmet = {
-  default_config = {
-    cmd = { 'ls_emmet', '--stdio' };
-    filetypes = { 'html', 'css', 'scss', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'haml',
-      'xml', 'xsl', 'pug', 'slim', 'sass', 'stylus', 'less', 'sss'};
-    root_dir = function(_)
-      return vim.loop.cwd()
-    end;
-    settings = {};
-  };
-}
+if not configs.ls_emmet then
+  configs.ls_emmet = {
+    default_config = {
+      cmd = {'ls_emmet', '--stdio'},
+      filetypes = {
+        'html', 'css', 'scss', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'haml', 'xml', 'xsl', 'pug', 'slim', 'sass',
+        'stylus', 'less', 'sss'
+      },
+      root_dir = function(fname)
+        return vim.loop.cwd()
+      end,
+      settings = {}
+    }
+  }
+end
 
 local system_name
 if vim.fn.has("mac") == 1 then
@@ -28,40 +33,31 @@ end
 
 -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
 -- vim.fn.stdpath('config') gives us ${HOME}/.config/nvim as a path
-local sumneko_root_path = vim.fn.stdpath('config')..'/lua-language-server'
-local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+local sumneko_root_path = vim.fn.stdpath('config') .. '/lua-language-server'
+local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name .. "/lua-language-server"
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
 -- Add any supported language server here after you install the corresponding languageserver.
-local langservers = {
-  'html',
-  'cssls',
-  'tsserver',
-  'pyright',
-  'gopls',
-  'rust_analyzer',
-  'ls_emmet',
-  'sumneko_lua'
-}
+local langservers = {'html', 'cssls', 'tsserver', 'pyright', 'gopls', 'rust_analyzer', 'ls_emmet', 'sumneko_lua'}
 
 for _, server in ipairs(langservers) do
   if server == 'sumneko_lua' then
     require'lspconfig'[server].setup {
-      cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+      cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
       settings = {
         Lua = {
           runtime = {
             -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
             version = 'LuaJIT',
             -- Setup your lua path
-            path = runtime_path,
+            path = runtime_path
           },
           diagnostics = {
             -- Get the language server to recognize the `vim` global
-            globals = {'vim'},
+            globals = {'vim'}
           },
           workspace = {
             -- Make the server aware of Neovim runtime files
@@ -69,16 +65,12 @@ for _, server in ipairs(langservers) do
             checkThirdParty = false
           },
           -- Do not send telemetry data containing a randomized but unique identifier
-          telemetry = {
-            enable = false,
-          },
-        },
-      },
+          telemetry = {enable = false}
+        }
+      }
     }
   else
-    require'lspconfig'[server].setup {
-      capabilities = capabilities
-    }
+    require'lspconfig'[server].setup {capabilities = capabilities}
   end
 end
 
