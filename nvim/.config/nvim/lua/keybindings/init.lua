ef963cab9236e4249bda49f30953cda468e646f8
Youngjae Lim <limyoungjae@gmail.com>
2021-12-01 15:41:18 -0500

    Added customized functions to telescope to find/grep notes

--- nvim/.config/nvim/lua/keybindings/init.lua
+++ nvim/.config/nvim/lua/keybindings/init.lua
@@ -2,10 +2,10 @@ vim.g.mapleader = ' '
 local map = vim.api.nvim_set_keymap
 
 -- Windows navigation horizontally/vertically
-map('n', '<left>', '<C-w>h',  { noremap = true, silent = false})
-map('n', '<right>', '<C-w>l', { noremap = true, silent = false})
-map('n', '<down>', '<C-w>j',  { noremap = true, silent = false})
-map('n', '<up>', '<C-w>k',    { noremap = true, silent = false})
+map('n', '<left>', '<C-w>h', {noremap = true, silent = false})
+map('n', '<right>', '<C-w>l', {noremap = true, silent = false})
+map('n', '<down>', '<C-w>j', {noremap = true, silent = false})
+map('n', '<up>', '<C-w>k', {noremap = true, silent = false})
 
 -- Toggle NvimTree
 map('n', '<leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent = false})
@@ -18,82 +18,61 @@ map('v', '>', '>gv', {noremap = true, silent = false})
 -- ** the Telescope comma maps **
 ---------------------------------
 -- Search in help
-map(
-  'n',
-  ',h',
-  [[<Cmd>lua require'telescope.builtin'.help_tags({results_title='Help Results'})<CR>]],
-  { noremap = true, silent = true }
-)
+map('n', ',h', [[<Cmd>lua require'telescope.builtin'.help_tags({results_title='Help Results'})<CR>]], {noremap = true, silent = true})
 -- Search keymaps
-map(
-  'n',
-  ',k',
-  [[<Cmd>lua require'telescope.builtin'.keymaps({results_title='Key Maps Results'})<CR>]],
-  { noremap = true, silent = true }
-)
+map('n', ',k', [[<Cmd>lua require'telescope.builtin'.keymaps({results_title='Key Maps Results'})<CR>]], {noremap = true, silent = true})
 -- Telescopic version of FZF's :Lines
-map(
-  "n",
-  ",l",
-  [[<Cmd>lua require('telescope.builtin').live_grep({grep_open_files=true})<CR>]],
-  { noremap = true, silent = true }
-)
+map("n", ",l", [[<Cmd>lua require('telescope.builtin').live_grep({grep_open_files=true})<CR>]], {noremap = true, silent = true})
 -- show LSP diagnostics for all open buffers
-map(
-  "n",
-  ",d",
-  [[<Cmd>lua require'telescope.builtin'.lsp_workspace_diagnostics()<CR>]],
-  { noremap = true, silent = true }
-)
+map("n", ",d", [[<Cmd>lua require'telescope.builtin'.lsp_workspace_diagnostics()<CR>]], {noremap = true, silent = true})
 -- Search in Marks
-map(
-  "n",
-  ",m",
-  [[<Cmd>lua require'telescope.builtin'.marks({results_title='Marks Results'})<CR>]],
-  { noremap = true, silent = true }
-)
+map("n", ",m", [[<Cmd>lua require'telescope.builtin'.marks({results_title='Marks Results'})<CR>]], {noremap = true, silent = true})
+-- Find notes
+map("n", ",n", [[<Cmd>lua require'telescope-config'.find_notes({results_title='Notes Results'})<CR>]], {noremap = true, silent = true})
+-- Grep notes
+map("n", ",g", [[<Cmd>lua require'telescope-config'.grep_notes({results_title='Notes Results'})<CR>]], {noremap = true, silent = true})
 
 -- Yank from the curent position to the end of line
-map("n", "Y", "y$", { noremap = true, silent = true })
+map("n", "Y", "y$", {noremap = true, silent = true})
 
 -- Keep it centered
-map("n", "n", "nzzzv", { noremap = true, silent = true })
-map("n", "N", "Nzzzv", { noremap = true, silent = true })
-map("n", "J", "mzJ`z", { noremap = true, silent = true })
+map("n", "n", "nzzzv", {noremap = true, silent = true})
+map("n", "N", "Nzzzv", {noremap = true, silent = true})
+map("n", "J", "mzJ`z", {noremap = true, silent = true})
 
 -- Undo break points
-map("i", ",", ",<c-g>u", { noremap = true, silent = true })
-map("i", ".", ".<c-g>u", { noremap = true, silent = true })
-map("i", "!", "!<c-g>u", { noremap = true, silent = true })
-map("i", "?", "?<c-g>u", { noremap = true, silent = true })
+map("i", ",", ",<c-g>u", {noremap = true, silent = true})
+map("i", ".", ".<c-g>u", {noremap = true, silent = true})
+map("i", "!", "!<c-g>u", {noremap = true, silent = true})
+map("i", "?", "?<c-g>u", {noremap = true, silent = true})
 
 -- run packer sync
-map("n", "<leader>ps", [[<Cmd>PackerSync<CR>]], { noremap = true, silent = true })
+map("n", "<leader>ps", [[<Cmd>PackerSync<CR>]], {noremap = true, silent = true})
 
 -- yank all in buffer (i.e., select all)
-map("n", "<leader>a", ":%y<cr>", { noremap = false, silent = true })
+map("n", "<leader>a", ":%y<cr>", {noremap = false, silent = true})
 
 -- Tab out of parenthesis, curly/square brackets
-map("i", "<C-o>", "<C-o>$", { noremap = false, silent = true })
+map("i", "<C-o>", "<C-o>$", {noremap = false, silent = true})
 
 -- Clear highlights searched
-map("n", "<esc><esc>", ":noh<CR>", { noremap = false, silent = true })
+map("n", "<esc><esc>", ":noh<CR>", {noremap = false, silent = true})
 
 -- open file in directory of current file
-map("n", "<leader>ob", ":e %:h/",  { noremap = false, silent = false })
-map("n", "<leader>ov", ":vs %:h/", { noremap = false, silent = false })
+map("n", "<leader>ob", ":e %:h/", {noremap = false, silent = false})
+map("n", "<leader>ov", ":vs %:h/", {noremap = false, silent = false})
 
 -- Open File Name under cursor in vert split
-map("n", "<leader>gf", ":vs <cfile><CR>", { noremap = false, silent = true })
+map("n", "<leader>gf", ":vs <cfile><CR>", {noremap = false, silent = true})
 
 -- Resize split windows
-map('n', '<S-left>', ':vertical resize -2<CR>',  { noremap = true, silent = false })
-map('n', '<S-down>', ':resize -2<CR>',           { noremap = true, silent = false })
-map('n', '<S-up>', ':resize +2<CR>',             { noremap = true, silent = false })
-map('n', '<S-right>', ':vertical resize +2<CR>', { noremap = true, silent = false })
+map('n', '<S-left>', ':vertical resize -2<CR>', {noremap = true, silent = false})
+map('n', '<S-down>', ':resize -2<CR>', {noremap = true, silent = false})
+map('n', '<S-up>', ':resize +2<CR>', {noremap = true, silent = false})
+map('n', '<S-right>', ':vertical resize +2<CR>', {noremap = true, silent = false})
 
 -- Easy align (type 'vipga' = this selects a paragraph and invokes :EasyAlign command)
-map("x", "ga", ":EasyAlign", { noremap = false, silent = true })
+map("x", "ga", ":EasyAlign", {noremap = false, silent = true})
 
 -- Terminal windows mappings
 function _G.set_terminal_keymaps()
--- nvim/.config/nvim/lua/telescope-config/init.lua
+++ nvim/.config/nvim/lua/telescope-config/init.lua
@@ -1,48 +1,69 @@
 local actions = require('telescope.actions')
 require('telescope').setup {
-    defaults = {
-        layout_config = {
-          width = 0.75,
-          prompt_position = "top",
-          preview_cutoff = 120,
-          horizontal = {mirror = false},
-          vertical = {mirror = false},
-        },
-        find_command = {'rg', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'},
-        prompt_prefix = " ",
-        selection_caret = " ",
-        entry_prefix = "  ",
-        initial_mode = "insert",
-        selection_strategy = "reset",
-        sorting_strategy = "ascending",
-        layout_strategy = "horizontal",
-        file_sorter = require'telescope.sorters'.get_fuzzy_file,
-        file_ignore_patterns = {},
-        generic_sorter =require'telescope.sorters'.get_generic_fuzzy_sorter,
-        path_display = {},
-        winblend = 0,
-        border = {},
-        borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
-        color_devicons = true,
-        use_less = true,
-        set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
-        file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-        grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-        qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-        buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
-        mappings = {
-            i = {
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-                ["<esc>"] = actions.close,
-                ["<CR>"] = actions.select_default + actions.center
-            },
-            n = {
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-            }
-        }
+  defaults = {
+    layout_config = {width = 0.75, prompt_position = "top", preview_cutoff = 120, horizontal = {mirror = false}, vertical = {mirror = false}},
+    find_command = {'rg', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'},
+    prompt_prefix = " ",
+    selection_caret = " ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "ascending",
+    layout_strategy = "horizontal",
+    file_sorter = require'telescope.sorters'.get_fuzzy_file,
+    file_ignore_patterns = {},
+    generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
+    path_display = {},
+    winblend = 0,
+    border = {},
+    borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
+    color_devicons = true,
+    use_less = true,
+    set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
+    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+        ["<esc>"] = actions.close,
+        ["<CR>"] = actions.select_default + actions.center
+      },
+      n = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist
+      }
     }
+  }
 }
+
+-- my telescopic customizations
+local M = {}
+
+-- @TODOUA: break up notes and configs
+function M.grep_notes()
+  local opts = {}
+  opts.hidden = true
+  -- opts.file_ignore_patterns = { 'thesaurus/'}
+  opts.search_dirs = {"~/Google Drive/My Drive/Notes/"}
+  opts.prompt_prefix = "   "
+  opts.prompt_title = " Grep Notes"
+  opts.path_display = {"smart"}
+  require("telescope.builtin").live_grep(opts)
+end
+
+function M.find_notes()
+  require("telescope.builtin").find_files {
+    prompt_title = " Find Notes",
+    path_display = {"smart"},
+    cwd = "~/Google Drive/My Drive/Notes/",
+    layout_strategy = "horizontal",
+    layout_config = {preview_width = 0.65, width = 0.75}
+  }
+end
+
+return M
