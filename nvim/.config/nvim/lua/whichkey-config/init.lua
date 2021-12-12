e72263abdc0a702b9e547e8d02e9fcf69cd51ed6
Youngjae Lim <limyoungjae@gmail.com>
2021-12-11 22:05:12 -0500

    Added a default config for lsp_signature

--- nvim/.config/nvim/lua/whichkey-config/init.lua
+++ nvim/.config/nvim/lua/whichkey-config/init.lua
@@ -1,18 +1,22 @@
 local wk = require("which-key")
+
 -- Floating terminal by toggleterm
 local Terminal = require('toggleterm.terminal').Terminal
 local toggle_float = function()
   local float = Terminal:new({direction = "float"})
   return float:toggle()
 end
+-- Floating lazygit by toggleterm
 local toggle_lazygit = function()
   local lazygit = Terminal:new({cmd = "lazygit", direction = "float"})
   return lazygit:toggle()
 end
+-- Floating notetaker by toggleterm
 local toggle_notetaker = function()
   local notetaker = Terminal:new({cmd = "notetaker.sh", direction = "float"})
   return notetaker:toggle()
 end
+
 -- @TODO: organize which-keys
 local mappings = {
   q = {":q<cr>", "Quit"},
@@ -27,14 +31,25 @@ local mappings = {
   l = {
     name = "LSP",
     i = {":LspInfo<cr>", "Connected Language Servers"},
-    A = {'<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', "Add workspace folder"},
-    R = {'<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', "Remove workspace folder"},
-    l = {'<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', "List workspace folder"},
+    A = {
+      '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', "Add workspace folder"
+    },
+    R = {
+      '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
+      "Remove workspace folder"
+    },
+    l = {
+      '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+      "List workspace folder"
+    },
     D = {'<cmd>lua vim.lsp.buf.type_definition()<CR>', "Type definition"},
     r = {'<cmd>lua vim.lsp.buf.rename()<CR>', "Rename"},
     h = {'<cmd>lua vim.lsp.buf.hover()<CR>', "Hover"},
     a = {'<cmd>lua vim.lsp.buf.code_action()<CR>', "Code actions"},
-    e = {'<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', "Show line diagnostics"},
+    e = {
+      '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
+      "Show line diagnostics"
+    },
     q = {'<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', "Show loclist"}
   },
   t = {
