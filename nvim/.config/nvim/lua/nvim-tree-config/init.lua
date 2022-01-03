1183dbbbdf5c784be79073e9c3907383597170d0
Youngjae Lim <limyoungjae@gmail.com>
2022-01-03 15:09:18 -0500

    Updated nvim-tree config

--- nvim/.config/nvim/lua/nvim-tree-config/init.lua
+++ nvim/.config/nvim/lua/nvim-tree-config/init.lua
@@ -1,72 +1,108 @@
+vim.g.nvim_tree_icons = {
+	default = "",
+	symlink = "",
+	git = {
+		unstaged = "",
+		staged = "S",
+		unmerged = "",
+		renamed = "➜",
+		deleted = "",
+		untracked = "U",
+		ignored = "◌",
+	},
+	folder = {
+		default = "",
+		open = "",
+		empty = "",
+		empty_open = "",
+		symlink = "",
+	},
+}
+
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not status_ok then
+	return
+end
+
+local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+if not config_status_ok then
+	return
+end
+
+local tree_cb = nvim_tree_config.nvim_tree_callback
+
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_width = 25
--- following options are the default
-require'nvim-tree'.setup {
-  gitignore = false,
-  -- disables netrw completely
-  disable_netrw = true,
-  -- hijack netrw window on startup
-  hijack_netrw = true,
-  -- open the tree when running this setup function
-  open_on_setup = false,
-  -- will not open on setup if the filetype is in this list
-  ignore_ft_on_setup = {},
-  -- closes neovim automatically when the tree is the last **WINDOW** in the view
-  auto_close = true,
-  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-  open_on_tab = false,
-  -- hijacks new directory buffers when they are opened.
-  update_to_buf_dir = {
-    -- enable the feature
-    enable = true,
-    -- allow to open the tree if it was previously closed
-    auto_open = true
-  },
-  -- hijack the cursor in the tree to put it at the start of the filename
-  hijack_cursor = true,
-  -- 🔥 updates the root directory of the tree on `DirChanged` (when you run `:cd` usually)
-  update_cwd = true,
-  -- show lsp diagnostics in the signcolumn
-  diagnostics = {
-    enable = false,
-    icons = {hint = "", info = "", warning = "", error = ""}
-  },
-  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-  update_focused_file = {
-    -- enables the feature
-    enable = false,
-    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-    -- only relevant when `update_focused_file.enable` is true
-    update_cwd = false,
-    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-    ignore_list = {}
-  },
-  -- configuration options for the system open command (`s` in the tree by default)
-  system_open = {
-    -- the command to run this, leaving nil should work in most cases
-    cmd = nil,
-    -- the command arguments as a list
-    args = {}
-  },
 
-  view = {
-    -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
-    width = 30,
-    -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
-    height = 30,
-    -- Hide the root path of the current folder on top of the tree
-    hide_root_folder = false,
-    -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-    side = 'left',
-    -- if true the tree will resize itself after opening a file
-    auto_resize = false,
-    mappings = {
-      -- custom only false will merge the list with the default mappings
-      -- if true, it will only use your list to set the mappings
-      custom_only = false,
-      -- list of mappings to set on the tree manually
-      list = {}
-    }
-  }
+-- following options are the default
+nvim_tree.setup {
+	gitignore = false,
+	-- disables netrw completely
+	disable_netrw = true,
+	-- hijack netrw window on startup
+	hijack_netrw = true,
+	-- open the tree when running this setup function
+	open_on_setup = false,
+	-- will not open on setup if the filetype is in this list
+	ignore_ft_on_setup = { "startify", "dashboard" },
+	-- closes neovim automatically when the tree is the last **WINDOW** in the view
+	auto_close = true,
+	-- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+	open_on_tab = false,
+	-- hijacks new directory buffers when they are opened.
+	update_to_buf_dir = {
+		-- enable the feature
+		enable = true,
+		-- allow to open the tree if it was previously closed
+		auto_open = true,
+	},
+	-- hijack the cursor in the tree to put it at the start of the filename
+	hijack_cursor = true,
+	-- 🔥 updates the root directory of the tree on `DirChanged` (when you run `:cd` usually)
+	update_cwd = true,
+	-- show lsp diagnostics in the signcolumn
+	diagnostics = {
+		enable = true,
+		icons = { hint = "", info = "", warning = "", error = "" },
+	},
+	-- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+	update_focused_file = {
+		-- enables the feature
+		enable = false,
+		-- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+		-- only relevant when `update_focused_file.enable` is true
+		update_cwd = false,
+		-- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+		-- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+		ignore_list = {},
+	},
+	-- configuration options for the system open command (`s` in the tree by default)
+	system_open = {
+		-- the command to run this, leaving nil should work in most cases
+		cmd = nil,
+		-- the command arguments as a list
+		args = {},
+	},
+
+	view = {
+		-- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
+		width = 30,
+		-- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
+		height = 30,
+		-- Hide the root path of the current folder on top of the tree
+		hide_root_folder = false,
+		-- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+		side = "left",
+		-- if true the tree will resize itself after opening a file
+		auto_resize = true,
+		mappings = {
+			-- custom only false will merge the list with the default mappings
+			-- if true, it will only use your list to set the mappings
+			custom_only = false,
+			-- list of mappings to set on the tree manually
+			list = {
+				{ key = "v", cb = tree_cb "vsplit" },
+			},
+		},
+	},
 }
