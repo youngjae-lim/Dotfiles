782cc2575d0013994010b19e7e53e22a5fa5751c
Youngjae Lim <limyoungjae@gmail.com>
2022-01-06 19:25:09 -0500

    Refactored toggleterm config

--- nvim/.config/nvim/lua/format-config/init.lua
+++ nvim/.config/nvim/lua/format-config/init.lua
@@ -1,93 +1,103 @@
-require'format'.setup {
-    -- Install prettier globally on your machine: npm install -g prettier
-    html = {{cmd = {"prettier -w"}}},
-    css = {{cmd = {"prettier -w"}}},
-    json = {{cmd = {"prettier -w"}}},
-    yaml = {{cmd = {"prettier -w"}}},
-    javascript = {{cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}},
-    javascriptreact = {
-        {cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}
-    },
-    typescript = {{cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}},
-    typescriptreact = {
-        {cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}
-    },
+-- TODO: delete this file if you decide to use null-ls as a formatting proxy going forward.
+require("format").setup {
+	-- Install prettier globally on your machine: npm install -g prettier
+	html = { { cmd = { "prettier -w" } } },
+	css = { { cmd = { "prettier -w" } } },
+	json = { { cmd = { "prettier -w" } } },
+	yaml = { { cmd = { "prettier -w" } } },
+	javascript = { { cmd = { "prettier -w", "./node_modules/.bin/eslint --fix" } } },
+	javascriptreact = {
+		{ cmd = { "prettier -w", "./node_modules/.bin/eslint --fix" } },
+	},
+	typescript = { { cmd = { "prettier -w", "./node_modules/.bin/eslint --fix" } } },
+	typescriptreact = {
+		{ cmd = { "prettier -w", "./node_modules/.bin/eslint --fix" } },
+	},
 
-    -- Install cmake: brew install cmake
-    -- Install luaformatter: luarocks install --server=https://luarocks.org/dev luaformatter
-    lua = {
-        {
-            cmd = {
-                function(file)
-                    return string.format(
-                               'lua-format -i --align-table-field --align-parameter --align-args --no-keep-simple-function-one-line --no-break-after-operator --column-limit=80 --break-after-table-lb --indent-width=2 %s',
-                               file)
-                end
-            }
-        }
-    },
-    -- Install black: pip install black
-    python = {
-        {
-            cmd = {
-                function(file)
-                    return string.format('black --quiet %s', file)
-                end
-            }
-        }
-    },
-    -- Install shfmt: go install mvdan.cc/sh/v3/cmd/shfmt@latest
-    sh = {
-        {cmd = {function(file) return string.format('shfmt -w %s', file) end}}
-    },
-    go = {
-        {
-            cmd = {function(file)
-                return string.format("gofmt -w %s", file)
-            end},
-            tempfile_postfix = ".tmp"
-        }
-    },
-    rust = {
-        {
-            cmd = {function(file)
-                return string.format("rustfmt %s", file)
-            end},
-            tempfile_postfix = ".tmp"
-        }
-    },
-    ruby = {
-        {
-            cmd = {function(file)
-                return string.format("rufo %s", file)
-            end},
-            tempfile_postfix = ".tmp"
-        }
-    },
-    -- Install vue-beautify: npm install -g vue-beautify
-    vue = {
-        {
-            cmd = {
-                function(file)
-                    return string.format("vue-beautify %s", file)
-                end
-            },
-            tempfile_postfix = ".tmp"
-        }
-    },
-    php = {
-        {
-            cmd = {
-                function(file)
-                    return string.format(
-                               "php-formatter formatter:use:sort --quiet %s",
-                               file)
-                end
-            },
-            tempfile_postfix = ".tmp"
-        }
-    }
+	-- Install cmake: brew install cmake
+	-- Install luaformatter: luarocks install --server=https://luarocks.org/dev luaformatter
+	lua = {
+		{
+			cmd = {
+				function(file)
+					return string.format(
+						"lua-format -i --align-table-field --align-parameter --align-args --no-keep-simple-function-one-line --no-break-after-operator --column-limit=80 --break-after-table-lb --indent-width=2 %s",
+						file
+					)
+				end,
+			},
+		},
+	},
+	-- Install black: pip install black
+	python = {
+		{
+			cmd = {
+				function(file)
+					return string.format("black --quiet %s", file)
+				end,
+			},
+		},
+	},
+	-- Install shfmt: go install mvdan.cc/sh/v3/cmd/shfmt@latest
+	sh = {
+		{ cmd = {
+			function(file)
+				return string.format("shfmt -w %s", file)
+			end,
+		} },
+	},
+	go = {
+		{
+			cmd = {
+				function(file)
+					return string.format("gofmt -w %s", file)
+				end,
+			},
+			tempfile_postfix = ".tmp",
+		},
+	},
+	rust = {
+		{
+			cmd = {
+				function(file)
+					return string.format("rustfmt %s", file)
+				end,
+			},
+			tempfile_postfix = ".tmp",
+		},
+	},
+	ruby = {
+		{
+			cmd = {
+				function(file)
+					return string.format("rufo %s", file)
+				end,
+			},
+			tempfile_postfix = ".tmp",
+		},
+	},
+	-- Install vue-beautify: npm install -g vue-beautify
+	vue = {
+		{
+			cmd = {
+				function(file)
+					return string.format("vue-beautify %s", file)
+				end,
+			},
+			tempfile_postfix = ".tmp",
+		},
+	},
+	php = {
+		{
+			cmd = {
+				function(file)
+					return string.format("php-formatter formatter:use:sort --quiet %s", file)
+				end,
+			},
+			tempfile_postfix = ".tmp",
+		},
+	},
 }
 
 -- To format on save
-vim.cmd('autocmd BufWritePost * FormatWrite')
+vim.cmd "autocmd BufWritePost * FormatWrite"
--- nvim/.config/nvim/lua/toggleterm-config/init.lua
+++ nvim/.config/nvim/lua/toggleterm-config/init.lua
@@ -1,58 +1,63 @@
-require("toggleterm").setup {
-  -- size can be a number or function which is passed the current terminal
-  function(term)
-    if term.direction == "horizontal" then
-      return 40
-    elseif term.direction == "vertical" then
-      return vim.o.columns * 0.4
-    end
-  end,
+local status_ok, toggleterm = pcall(require, "toggleterm")
+if not status_ok then
+	return
+end
 
-  open_mapping = [[<c-\>]],
-  hide_numbers = true, -- hide the number column in toggleterm buffers
-  shade_filetypes = {},
-  shade_terminals = true,
-  start_in_insert = true,
-  insert_mappings = true, -- whether or not the open mapping applies in insert mode
-  persist_size = true,
-  direction = 'horizontal',
-  close_on_exit = true, -- close the terminal window when the process exits
-  shell = vim.o.shell -- change the default shell
+toggleterm.setup {
+	-- size can be a number or function which is passed the current terminal
+	function(term)
+		if term.direction == "horizontal" then
+			return 40
+		elseif term.direction == "vertical" then
+			return vim.o.columns * 0.4
+		end
+	end,
+
+	open_mapping = [[<c-\>]],
+	hide_numbers = true, -- hide the number column in toggleterm buffers
+	shade_filetypes = {},
+	shade_terminals = true,
+	start_in_insert = true,
+	insert_mappings = true, -- whether or not the open mapping applies in insert mode
+	persist_size = true,
+	direction = "horizontal",
+	close_on_exit = true, -- close the terminal window when the process exits
+	shell = vim.o.shell, -- change the default shell
 }
 
 -- Terminal windows mappings
 -- It can be helpful to add mappings to make moving in and out of a terminal easier once toggled, whilst still keeping it open.
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+	local opts = { noremap = true }
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
+vim.cmd "autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()"
 
 -- Floating terminal by toggleterm
-local Terminal = require('toggleterm.terminal').Terminal
+local Terminal = require("toggleterm.terminal").Terminal
 
 -- Floating terminal by toggleterm
-local float = Terminal:new({direction = "float"})
+local float = Terminal:new { direction = "float" }
 
 _TOGGLE_FLOAT = function()
-  float:toggle()
+	float:toggle()
 end
 
 -- Floating lazygit by toggleterm
-local lazygit = Terminal:new({cmd = "lazygit", direction = "float"})
+local lazygit = Terminal:new { cmd = "lazygit", direction = "float" }
 
 _TOGGLE_LAZYGIT = function()
-  lazygit:toggle()
+	lazygit:toggle()
 end
 
 -- Floating notetaker by toggleterm
-local notetaker = Terminal:new({cmd = "notetaker.sh", direction = "float"})
+local notetaker = Terminal:new { cmd = "notetaker.sh", direction = "float" }
 
 _TOGGLE_NOTETAKER = function()
-  notetaker:toggle()
+	notetaker:toggle()
 end
