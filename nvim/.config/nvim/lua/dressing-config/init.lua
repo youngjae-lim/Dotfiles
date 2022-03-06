9e2a0077adc3691ce2c3f561ffd141fab3fa74de
Youngjae Lim <limyoungjae@gmail.com>
2022-03-05 21:02:08 -0500

    Removed deprecated row and col variables from dressing-config

--- nvim/.config/nvim/lua/dressing-config/init.lua
+++ nvim/.config/nvim/lua/dressing-config/init.lua
@@ -1,77 +1,73 @@
-require('dressing').setup({
-  input = {
-    -- Default prompt string
-    default_prompt = "➤ ",
+require("dressing").setup {
+	input = {
+		-- Default prompt string
+		default_prompt = "➤ ",
 
-    -- When true, <Esc> will close the modal
-    insert_only = true,
+		-- When true, <Esc> will close the modal
+		insert_only = true,
 
-    -- These are passed to nvim_open_win
-    anchor = "SW",
-    relative = "cursor",
-    row = 0,
-    col = 0,
-    border = "rounded",
+		-- These are passed to nvim_open_win
+		anchor = "SW",
+		relative = "cursor",
+		border = "rounded",
 
-    -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-    prefer_width = 40,
-    max_width = nil,
-    min_width = 20,
+		-- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+		prefer_width = 40,
+		max_width = nil,
+		min_width = 20,
 
-    -- Window transparency (0-100)
-    winblend = 10,
+		-- Window transparency (0-100)
+		winblend = 10,
 
-    -- see :help dressing-prompt
-    prompt_buffer = false,
+		-- see :help dressing-prompt
+		prompt_buffer = false,
 
-    -- see :help dressing_get_config
-    get_config = nil
-  },
-  select = {
-    -- Priority list of preferred vim.select implementations
-    backend = {"telescope", "fzf", "builtin", "nui"},
+		-- see :help dressing_get_config
+		get_config = nil,
+	},
+	select = {
+		-- Priority list of preferred vim.select implementations
+		backend = { "telescope", "fzf", "builtin", "nui" },
 
-    -- Options for telescope selector
-    telescope = {
-      -- can be 'dropdown', 'cursor', or 'ivy'
-      theme = "dropdown"
-    },
+		-- Options for telescope selector
+		telescope = {
+			-- can be 'dropdown', 'cursor', or 'ivy'
+			theme = "dropdown",
+		},
 
-    -- Options for fzf selector
-    fzf = {window = {width = 0.5, height = 0.4}},
+		-- Options for fzf selector
+		fzf = { window = { width = 0.5, height = 0.4 } },
 
-    -- Options for nui Menu
-    nui = {
-      position = "50%",
-      size = nil,
-      relative = "editor",
-      border = {style = "rounded"},
-      max_width = 80,
-      max_height = 40
-    },
+		-- Options for nui Menu
+		nui = {
+			position = "50%",
+			size = nil,
+			relative = "editor",
+			border = { style = "rounded" },
+			max_width = 80,
+			max_height = 40,
+		},
 
-    -- Options for built-in selector
-    builtin = {
-      -- These are passed to nvim_open_win
-      anchor = "NW",
-      relative = "cursor",
-      row = 0,
-      col = 0,
-      border = "rounded",
+		-- Options for built-in selector
+		builtin = {
+			-- These are passed to nvim_open_win
+			anchor = "NW",
+			relative = "cursor",
+			border = "rounded",
 
-      -- Window transparency (0-100)
-      winblend = 10,
+			-- Window transparency (0-100)
+			winblend = 10,
 
-      -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-      width = nil,
-      max_width = 0.8,
-      min_width = 40,
-      height = nil,
-      max_height = 0.9,
-      min_height = 10
-    },
+			-- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+			width = nil,
+			max_width = 0.8,
+			min_width = 40,
+			height = nil,
+			max_height = 0.9,
+			min_height = 10,
+		},
 
-    -- see :help dressing_get_config
-    get_config = nil
-  }
-})
+		-- see :help dressing_get_config
+		get_config = nil,
+	},
+}
